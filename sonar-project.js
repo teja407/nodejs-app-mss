509696bb9b20bacd84db2d82634b5a577978bd3a
root <root@nodejs-server.us-central1-a.c.iron-tea-309412.internal>
2021-05-14 12:51:28 +0000

    added new changes

--- sonar-project.js
+++ sonar-project.js
@@ -1,17 +1,23 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
+//http://104.154.90.172:9000/
+
 sonarqubeScanner({
-  serverUrl: 'http://18.221.74.112:9000/',
+  serverUrl: 'http://104.154.90.172:9000/',
        options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
+	       'sonar.projectKey':'nodejsapp',
+	       'sonar.sources':'.',
+	       'sonar.login':'5a959c3e5ec67f6c287ccc6edaef9e57ac4b90ac',
+	     //  'sonar.host':'http://104.154.90.172:9000',
+	    //'sonar.projectDescription': 'This is a Node JS application',
+	    //'sonar.projectName': 'Node JS Application - Sample',
+	    //'sonar.projectKey':'NodeJsMSS',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            //'sonar.projectVersion':'1.0',
+	    //'sonar.language':'js',
+            //'sonar.sourceEncoding':'UTF-8',
+            //'sonar.sources': '.',
 	  //'sonar.tests': 'specs',
           //'sonar.inclusions' : 'src/**'
        },
